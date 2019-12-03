425a5adb8700989e8b15d19ba2d17d5cb94c8b53
Matthew J. Martin <matthew.mar10@gmail.com>
2019-12-03 15:20:46 +0700

    use variables for address inputs

--- example.js
+++ example.js
@@ -5,7 +5,9 @@ const Runner = require('./');
 
 const state = {
   $inputs: {
-    trustee: '0x1f9c410d5562bb6590b8f891f2e26311f9a6ef8c'
+    trustee: '0x1f9c410d5562bb6590b8f891f2e26311f9a6ef8c',
+    operations: '0x739b62a44357115c483098e33eb119a388a793a1',
+    exchange: '0xc10c15ad37024f724e0c7efb3a284125efb69890',
   },
   $outputs: {},
   $contracts: {},
@@ -42,13 +44,31 @@ const tasks = [{
     fiatMinimumInvestmentAmount: 10000000,
     fiatPerEthAmount: 1735653,
     maturityLengthDays: 365,
-    fiatExchangeAccount: '0xc10c15ad37024f724e0c7efb3a284125efb69890',
-    managementAccount: '0x1f9c410d5562bb6590b8f891f2e26311f9a6ef8c',
-    operationsAccount: '0x739b62a44357115c483098e33eb119a388a793a1',
+    fiatExchangeAccount: '$inputs.exchange',
+    managementAccount: '$inputs.trustee',
+    operationsAccount: '$inputs.operations',
   }, {
     from: '$inputs.trustee',
   }],
   outputs: ['$deployed.token']
+}, {
+  description: 'Depoy a new Profits contract',
+  contract: 'Profits',
+  run: 'new',
+  inputs: ['$deployed.token.address', {
+    from: '$inputs.trustee',
+    gas: 3e6,
+    gasPrice: 2e9
+  }],
+  outputs: ['$deployed.profits']
+}, {
+  description: 'Link Token to Profits',
+  contract: 'Token',
+  at: '$deployed.token.address',
+  run: 'setProfitsContract',
+  inputs: ['$deployed.profits.address', {
+    from: '$inputs.trustee'
+  }]
 }];
 
 const runner = new Runner({
@@ -56,13 +76,30 @@ const runner = new Runner({
   workingDirectory: path.join(__dirname, '/../../Blossom/smartsukuk-dual-mudaraba')
 });
 
+// const task = {
+//   description: 'Deploy a new SafeMathLib',
+//   contract: 'SafeMathLib',
+//   run: 'new',
+//   inputs: [{
+//     from: '$inputs.trustee'
+//   }],
+//   outputs: '$deployed.safeMathLib'
+// };
+
 (async () => {
   try {
     const results = await runner.run(tasks, state);
+    // const results = await runner.run(task, state);
     // console.log('State:', state);
-    console.log('Results:', results);
+    // console.log('Results:', results);
+    // console.log('Requests:');
+    // console.log(process._getActiveRequests());
+    // const handles = process._getActiveHandles();
+    // console.log('Handles: ', handles.length);
+    // handles.forEach(handle => {
+    //   console.log(handle);
+    // });
   } catch (err) {
     console.error('Failed with error:', err);
   }
-
 })();
